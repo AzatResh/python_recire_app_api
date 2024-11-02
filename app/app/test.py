1c06b213b9290661cc86ba73828c6a0d4e30d151
AzatResh <Azat.R.2003@mail.ru>
2024-11-02 19:49:40 +0500

    configure database

---
+++ app/app/test.py
@@ -0,0 +1,18 @@
+# """
+#     Sample Test
+# """
+#
+# from django.test import SimpleTestCase
+# from . import calc
+#
+# class CalcTest(SimpleTestCase):
+#
+#     def test_add_numbers(self):
+#         result = calc.add(5, 6)
+#
+#         self.assertEquals(result, 11)
+#
+#     def test_subtract_numbers(self):
+#         result = calc.subtract(10, 15)
+#
+#         self.assertEquals(result, 5)
