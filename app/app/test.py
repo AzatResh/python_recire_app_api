a18ef7c2519c65080e5c35908f1939c0be543e95
AzatResh <Azat.R.2003@mail.ru>
2024-11-02 19:59:36 +0500

    debug test functions

--- app/app/test.py
+++ app/app/test.py
@@ -1,18 +1,18 @@
-# """
-#     Sample Test
-# """
-#
-# from django.test import SimpleTestCase
-# from . import calc
-#
-# class CalcTest(SimpleTestCase):
-#
-#     def test_add_numbers(self):
-#         result = calc.add(5, 6)
-#
-#         self.assertEquals(result, 11)
-#
-#     def test_subtract_numbers(self):
-#         result = calc.subtract(10, 15)
-#
-#         self.assertEquals(result, 5)
+"""
+    Sample Test
+"""
+
+from django.test import SimpleTestCase
+from . import calc
+
+class CalcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        result = calc.add(5, 6)
+
+        self.assertEqual(result, 11)
+
+    def test_subtract_numbers(self):
+        result = calc.subtract(10, 15)
+
+        self.assertEqual(result, 5)
